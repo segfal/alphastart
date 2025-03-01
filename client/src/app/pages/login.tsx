13866bd6ff9de23c81a18445a8a3fadace5e9f25
Hafeefa <hafeefa40@gmail.com>
2025-03-01 16:23:32 -0500

    redid nextjs download, added login files

---
+++ client/src/app/pages/login.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React, { useState } from 'react';
+import styles from './login.module.css';
+
+const Login: React.FC = () => {
+  return (
+    <div className={styles.container}>
+    </div>
+  );
+};
+
+export default Login;
